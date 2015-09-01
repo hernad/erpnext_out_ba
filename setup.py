64c662e18af3b93420739557fce66d471cb4824c
Ernad Husremović <hernad@bring.out.ba>
2015-09-01 19:03:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.0.1'
+version = '0.0.2'
 
 setup(
-    name='erpnext.out.ba',
+    name='erpnext_out_ba',
     version=version,
     description='ERPNext.out.ba website',
     author='bring.out.ba',
